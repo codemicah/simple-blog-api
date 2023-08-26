bac60955a7fdc28697bada9c6406690735b67059
Micah Elijah <melijah200@gmail.com>
2023-08-26 09:27:09 +0100

    Add wildcard path

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import userRouter from "./routes/user";
@@ -16,5 +16,16 @@ app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
+
+app.use("*", (req: Request, res: Response) => {
+  const path = req.originalUrl;
+  const method = req.method;
+  return res.status(404).json({
+    error: true,
+    path,
+    method,
+    message: `The method ${method} is not defined on path ${path}`,
+  });
+});
 
 export default app;
