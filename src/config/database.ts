1b86ecd0e44b885bcfce81c42eb6ba4d2ec769d2
Micah Elijah <melijah200@gmail.com>
2023-08-26 01:46:17 +0100

    Do not compile test folder

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,4 @@
-import { Pool, QueryConfig } from "pg";
+import { Pool, PoolClient, QueryConfig } from "pg";
 import { config } from "./env";
 import { userSchema } from "../models/user";
 import { postSchema } from "../models/post";
@@ -19,13 +19,8 @@ pool.on("error", (err, _client) => {
   process.exit(-1);
 });
 
-pool.on("connect", (client) => {
-  // create Users table
-  client.query(userSchema);
-  // create Posts table
-  client.query(postSchema);
-  // create Comments table
-  client.query(commentSchema);
+pool.on("connect", async (client) => {
+  await setUpSchemas(client);
 });
 
 // function to facilitate queries
@@ -33,5 +28,24 @@ const dbQuery = async (query: QueryConfig) => {
   const response = await pool.query(query);
   return response.rows;
 };
+
+async function setUpSchemas(client: PoolClient) {
+  // create Users table
+  client.query(userSchema);
+  // create Posts table
+  client.query(postSchema);
+  // create Comments table
+  client.query(commentSchema);
+  // create indexes
+  client.query({
+    text: `CREATE INDEX IF NOT EXISTS idx_total_posts ON Users (totalPosts)`,
+  });
+  client.query({
+    text: `CREATE INDEX IF NOT EXISTS idx_post_author ON Posts (author)`,
+  });
+  client.query({
+    text: `CREATE INDEX IF NOT EXISTS idx_comment ON Comments (createdAt, userId, postId)`,
+  });
+}
 
 export default dbQuery;
